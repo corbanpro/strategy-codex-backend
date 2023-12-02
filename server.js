a70e7ca14b1c6b096333032d61806fcff1db1a28
corbanpro <corbanpro@gmail.com>
2023-12-02 01:03:54 -0700

    server backs up summaries every hour

--- server.js
+++ server.js
@@ -3,15 +3,43 @@ const cors = require("cors");
 const fs = require("fs");
 const summaries = require("./summaries.json");
 
+const folderName = "./backups";
+try {
+  if (!fs.existsSync(folderName)) {
+    fs.mkdirSync(folderName);
+  }
+} catch (err) {
+  console.error(err);
+}
+
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+version = "1";
+const backupSummaries = () => {
+  fs.writeFile(`./backups/summaries(${version}).json`, JSON.stringify(summaries), (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+  console.log("summary backup complete - " + version + " - " + new Date());
+  version++;
+};
+
+setInterval(backupSummaries, 1000 * 60 * 60);
 
 app.get("/test", (req, res) => {
   res.send({ message: "success" });
 });
 
 app.get("/getsummaries", (req, res) => {
+  res.send(summaries);
+});
+
+app.get("/getsummaries/:version_num", (req, res) => {
+  const version = req.params.version_num;
+  const summaries = require(`./backups/summaries(${version}).json`);
   res.send(summaries);
 });
 
