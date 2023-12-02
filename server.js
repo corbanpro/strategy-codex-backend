c13d5b753efe60cfea0c374d37cd2c407562dab8
corbanpro <corbanpro@gmail.com>
2023-12-02 02:29:53 -0700

    tokens

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const fs = require("fs");
 const summaries = require("./summaries.json");
 const versions = require("./versions.json");
 
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// if there is no backups folder, create one
 const folderName = "./backups";
 try {
   if (!fs.existsSync(folderName)) {
@@ -13,10 +18,7 @@ try {
   console.error(err);
 }
 
-const app = express();
-app.use(express.json());
-app.use(cors());
-
+//backup the summaries every 6 hours
 let versionNum = Object.keys(versions).length + 1;
 const backupSummaries = () => {
   fs.writeFile(`./backups/summaries(${versionNum}).json`, JSON.stringify(summaries), (err) => {
@@ -34,24 +36,27 @@ const backupSummaries = () => {
   });
   versionNum++;
 };
-
 setInterval(backupSummaries, 1000 * 60 * 60 * 6);
 
+// test endpoint
 app.get("/test", (req, res) => {
   res.send({ message: "success" });
   console.log("test");
 });
 
+// get latest summaries
 app.get("/getsummaries", (req, res) => {
   res.send(summaries);
   console.log("getSummaries");
 });
 
+//get a list of version numbers and update dates
 app.get("/versions", (req, res) => {
   res.send({ versions });
   console.log("versions");
 });
 
+//get a specific version of summaries
 app.get("/getsummaries/:version_num", (req, res) => {
   const version = req.params.version_num;
   const summaries = require(`./backups/summaries(${version}).json`);
@@ -59,6 +64,7 @@ app.get("/getsummaries/:version_num", (req, res) => {
   console.log("getSummaries/version");
 });
 
+// set summaries
 app.post("/setsummaries", (req, res) => {
   summaries[req.body.id].summary = req.body.summary;
   fs.writeFile("./summaries.json", JSON.stringify(summaries), (err) => {
@@ -68,6 +74,33 @@ app.post("/setsummaries", (req, res) => {
   });
   res.send("summaries updated");
   console.log("setSummaries");
+});
+
+//authenticate user
+const token = "asibukj4bt283y9827305ih3o8reu94hy57tuirjnb3geyrf78v7ygfruej3mn4rht";
+const username = "ilabz";
+const password = "password";
+
+app.post("/checktoken", (req, res) => {
+  if (req.body.token === token) {
+    res.send({ message: "success", token: token });
+  } else {
+    res.send({ message: "access denied", token: req.body.token });
+  }
+  console.log("checkToken");
+});
+
+app.post("/gettoken", (req, res) => {
+  if (req.body.username === username) {
+    if (req.body.password === password) {
+      res.send({ message: "success", token: token });
+    } else {
+      res.send({ message: "incorrect password", token: "" });
+    }
+  } else {
+    res.send({ message: "incorrect username", token: "" });
+  }
+  console.log("getToken");
 });
 
 app.listen(8080, () => console.log("API is running on port 8080"));
