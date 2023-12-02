ceae2f586004e0183067b3ebd5fd71a3731b77bb
corbanpro <corbanpro@gmail.com>
2023-12-01 19:36:31 -0700

    another commit

--- server.js
+++ server.js
@@ -23,3 +23,5 @@ app.post("/setsummaries", (req, res) => {
 
 app.listen(8080, () => console.log("API is running on http://localhost:8080"));
 //https://www.youtube.com/watch?v=7CqJlxBYj-M
+
+// Path: client/src/App.js
